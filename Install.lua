6ec8ecb09935fc208fb19b8de153472c9fe51962
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-08-24 18:03:00 -0700

    DataStore2 Installer

--- Install.lua
+++ Install.lua
@@ -3,19 +3,6 @@
 local HttpService = game:GetService("HttpService")
 local ServerScriptService = game:GetService("ServerScriptService")
 
-local DataStore2 = Instance.new("ModuleScript")
-local Verifier = Instance.new("ModuleScript")
-local TableUtil = Instance.new("ModuleScript")
-local Settings = Instance.new("ModuleScript")
-local Promise = Instance.new("ModuleScript")
-local IsPlayer = Instance.new("ModuleScript")
-local DataStoreServiceRetriever = Instance.new("ModuleScript")
-local Constants = Instance.new("ModuleScript")
-
-local SavingMethods = Instance.new("ModuleScript")
-local Standard = Instance.new("ModuleScript")
-local OrderedBackups = Instance.new("ModuleScript")
-
 local function Install(Name, Url, Parent)
 	local ModuleScript = Instance.new("ModuleScript")
 	
