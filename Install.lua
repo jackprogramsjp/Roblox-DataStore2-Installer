97e9636fd696089bb3332490674ef65ef850fdf4
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2020-08-24 17:59:10 -0700

    DataStore2 Installer

--- Install.lua
+++ Install.lua
@@ -16,6 +16,22 @@ local SavingMethods = Instance.new("ModuleScript")
 local Standard = Instance.new("ModuleScript")
 local OrderedBackups = Instance.new("ModuleScript")
 
-DataStore2.Source = HttpService:GetAsync("https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/init.lua")
+local function Install(Name, Url, Parent)
+	DataStore2.Name = Name
+	DataStore2.Source = HttpService:GetAsync(Url)
+	DataStore2.Parent = Parent
+end
 
-DataStore2.Parent = ServerScriptService
+Install("DataStore2", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/init.lua", ServerScriptService)
+Install("Verifier", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/Verifier.lua", DataStore2)
+Install("TableUtil", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/TableUtil.lua", DataStore2)
+Install("Settings", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/Settings.lua", DataStore2)
+Install("Promise", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/Promise.lua", DataStore2)
+Install("IsPlayer", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/IsPlayer.lua", DataStore2)
+Install("DataStoreServiceRetriever", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/DataStoreServiceRetriever.lua", DataStore2)
+Install("Constants", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/Constants.lua", DataStore2)
+Install("SavingMethods", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/SavingMethods/init.lua", DataStore2)
+Install("Standard", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/SavingMethods/Standard.lua", SavingMethods)
+Install("OrderedBackups", "https://raw.githubusercontent.com/Kampfkarren/Roblox/master/DataStore2/SavingMethods/OrderedBackups.lua", SavingMethods)
+
+print("Installed DataStore2")
